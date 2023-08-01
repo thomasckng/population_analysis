01bba3551754a46ed2713d75c187cfff9807b18d
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-08-01 11:49:58 +0800

    Add corner plot

--- PE_exercise/PE_Gaussian/PE_Gaussian.py
+++ PE_exercise/PE_Gaussian/PE_Gaussian.py
@@ -26,46 +26,91 @@ class Inference(cpnest.model.Model):
         logL = np.sum(np.log(normal_distribution(self.samples, x['mu'], x['sigma'])))
         return logL
 
-if __name__ == '__main__':
-    mu = np.random.uniform(-10, 10)
-    sigma = np.random.uniform(0, 5)
-    npts = 100
-    
-    postprocess = False
 
-    if not postprocess:
-        # draw samples from a normal distribution
-        samples = np.random.normal(mu, sigma, npts)
-        np.savetxt('samples.txt', samples)
+mu = np.random.uniform(-10, 10)
+sigma = np.random.uniform(0, 5)
+npts = 100
+
+postprocess = True
+
+# Generate points
+if not postprocess:
+    samples = np.random.normal(mu, sigma, npts)
+    np.savetxt('samples.txt', samples)
+    with open('true_values.txt', 'w') as f:
+        f.write('mu = '+str(mu)+'\n')
+        f.write('sigma = '+str(sigma)+'\n')
+else:
+    samples = np.loadtxt('samples.txt')
+    with open('true_values.txt', 'r') as f:
+        lines = f.readlines()
+        mu = float(lines[0].split('=')[1])
+        sigma = float(lines[1].split('=')[1])
+
+# Inference
+W = Inference(samples)
+if not postprocess:
+    work = cpnest.CPNest(W, verbose = 2, output = 'inference/', nnest = 1, nensemble = 1, nlive = 1000)
+    work.run()
+    post = work.posterior_samples.ravel()
+else:
+    with h5py.File('./inference/cpnest.h5', 'r') as f:
+        post = f['combined']['posterior_samples'][()]
+samps = np.column_stack([post[lab] for lab in ['mu', 'sigma']])
+    
+# Plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import sys
+sys.path.append('../../')
+from kde_contour import kdeplot_2d_clevels, Bounded_1d_kde
+sns.set_theme(palette='colorblind')
+
+def kdeplot2d(x, y, rng=12345, **kws):
+    kws.pop('label', None)
+    kdeplot_2d_clevels(xs=x, ys=y, auto_bound=True, rng=rng, **kws)
+
+def kdeplot1d(x, **kws):
+    if np.all(x.isna()):
+        return
+    for key in ['label', 'hue_order', 'color']:
+        kws.pop(key, None)
+    df = pd.DataFrame({'x': x, 'y': Bounded_1d_kde(x, xlow=min(x), xhigh=max(x), **kws)(x)})
+    df = df.sort_values(['x'])
+    plt.fill_between(df['x'], df['y'], np.zeros(len(x)), alpha=0.2)
+    plt.plot(df['x'], df['y'])
+    plt.xlim(df['x'].min(), df['x'].max())
+    current_ymax = plt.ylim()[1]
+    if current_ymax > df['y'].max()*1.05:
+        plt.ylim(0,current_ymax)
     else:
-        samples = np.loadtxt('samples.txt')
-    
-    W = Inference(samples)
-    if not postprocess:
-        work = cpnest.CPNest(W, verbose = 2, output = 'inference/', nnest = 1, nensemble = 1, nlive = 1000)
-        work.run()
-        post = work.posterior_samples.ravel()
-    else:
-        with h5py.File('./inference/cpnest.h5', 'r') as f:
-            post = f['combined']['posterior_samples'][()]
-    samps   = np.column_stack([post[lab] for lab in ['mu', 'sigma']])
-    
-    # Plotting
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    sns.set_palette('colorblind')
+        plt.ylim(0,df['y'].max()*1.05)
 
-    # Plot samples histogram
-    plt.hist(samples, bins = 20, density = True, color = sns.color_palette()[0], label = 'Data')
+df = pd.DataFrame(samps, columns = ['mu', 'sigma'])
+vars = ['mu', 'sigma']
+g = sns.PairGrid(df,
+                 vars=vars,
+                 corner=True,
+                 diag_sharey=False,
+                 layout_pad=0.
+                )
 
-    # Plot Gaussian with true parameters
-    x = np.linspace(-30, 30, 500)
-    plt.plot(x, normal_distribution(x, mu, sigma), color = sns.color_palette()[3], label = 'True')
+g.map_lower(kdeplot2d, levels=[0.90,0.3935])
+g.map_diag(kdeplot1d) 
 
-    # Plot Gaussian with maximum likelihood parameters
-    max_likelihood = post[np.argmax(post['logL'])]
-    plt.plot(x, normal_distribution(x, max_likelihood[0], max_likelihood[1]), color = sns.color_palette()[4], linestyle = '--', label = 'Max L')
+for i in range(len(vars)):
+    for j in range(i):
+        g.axes[i,j].set_xlim(df[vars[j]].min(), df[vars[j]].max())
+        g.axes[i,j].set_ylim(df[vars[i]].min(), df[vars[i]].max())
 
-    # Save plot
-    plt.legend()
-    plt.savefig('inference/PE_Gaussian.pdf')
+g.axes[1,0].set_xlabel(r"$\mu$")
+g.axes[1,0].set_ylabel(r"$\sigma$")
+g.axes[1,1].set_xlabel(r"$\mu$")
+
+g.axes[0,0].axvline(mu, color = 'k', linestyle = '--')
+g.axes[1,0].axvline(mu, color = 'k', linestyle = '--')
+g.axes[1,0].axhline(sigma, color = 'k', linestyle = '--')
+g.axes[1,1].axvline(sigma, color = 'k', linestyle = '--')
+
+g.savefig('inference/PE_Gaussian.pdf', dpi=300)
--- PE_exercise/fit_line/fit_straight_line.py
+++ PE_exercise/fit_line/fit_straight_line.py
@@ -29,48 +29,59 @@ class Inference(cpnest.model.Model):
         logL = np.sum(np.log(normal_distribution(self.pts[:,1], x['m']*self.pts[:,0] + x['c'], sigma)))
         return logL
 
-if __name__ == '__main__':
-    m = np.random.uniform(-10, 10)
-    c = np.random.uniform(-10, 10)
-    npts = 10
-    
-    postprocess = False
+m = np.random.uniform(-10, 10)
+c = np.random.uniform(-10, 10)
+npts = 10
 
-    if not postprocess:
-        x = np.random.uniform(-10, 10, size = npts)
-        y = m*x + c + np.random.normal(0, sigma, size = npts)
-        pts = np.column_stack([x, y])
-        np.savetxt('points.txt', pts)
-    else:
-        pts = np.loadtxt('points.txt')
+postprocess = True
 
-    W = Inference(pts)
-    if not postprocess:
-        work = cpnest.CPNest(W, verbose = 2, output = 'inference/', nnest = 1, nensemble = 1, nlive = 1000)
-        work.run()
-        post = work.posterior_samples.ravel()
-        samps = np.column_stack([post[lab] for lab in ['m', 'c']])
-    else:
-        with h5py.File('./inference/cpnest.h5', 'r') as f:
-            post = f['combined']['posterior_samples'][()]
-    samps   = np.column_stack([post[lab] for lab in ['m', 'c']])
-    
-    # Plotting
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    sns.set_palette('colorblind')
+# Generate points
+if not postprocess:
+    x = np.random.uniform(-10, 10, size = npts)
+    y = m*x + c + np.random.normal(0, sigma, size = npts)
+    pts = np.column_stack([x, y])
+    np.savetxt('points.txt', pts)
+    with open('true_values.txt', 'w') as f:
+        f.write('m = '+str(m)+'\n')
+        f.write('c = '+str(c)+'\n')
+else:
+    pts = np.loadtxt('points.txt')
+    with open('true_values.txt', 'r') as f:
+        lines = f.readlines()
+        m = float(lines[0].split('=')[1])
+        c = float(lines[1].split('=')[1])
 
-    # Plot points
-    plt.scatter(pts[:,0], pts[:,1], color = sns.color_palette()[0], label = 'Data')
+# Inference
+W = Inference(pts)
+if not postprocess:
+    work = cpnest.CPNest(W, verbose = 2, output = 'inference/', nnest = 1, nensemble = 1, nlive = 1000)
+    work.run()
+    post = work.posterior_samples.ravel()
+else:
+    with h5py.File('./inference/cpnest.h5', 'r') as f:
+        post = f['combined']['posterior_samples'][()]
+samps = np.column_stack([post[lab] for lab in ['m', 'c']])
 
-    # Plot straight line with true parameters
-    x = np.linspace(-10, 10, 100)
-    plt.plot(x, m*x + c, color = sns.color_palette()[3], label = 'True')
+# Plotting
+import seaborn as sns
+import pandas as pd
+sns.set_theme(palette='colorblind')
 
-    # Plot straight line with maximum likelihood parameters
-    max_likelihood = post[np.argmax(post['logL'])]
-    plt.plot(x, max_likelihood[0]*x + max_likelihood[1], color = sns.color_palette()[4], linestyle = '--', label = 'Max L')
+df = pd.DataFrame(samps, columns = ['m', 'c'])
+g = sns.PairGrid(df,
+                 vars = ['m', 'c'],
+                 diag_sharey=False,
+                 corner=True)
+g.map_diag(sns.kdeplot, fill=True)
+g.map_lower(sns.kdeplot, levels=[1-0.9, 1-0.3935])
 
-    # Save figure
-    plt.legend()
-    plt.savefig('inference/fit_straight_line.pdf')
+g.axes[1,0].set_xlabel(r'$m$')
+g.axes[1,0].set_ylabel(r'$c$')
+g.axes[1,1].set_xlabel(r'$c$')
+
+g.axes[0,0].axvline(m, color = 'k', linestyle = '--')
+g.axes[1,0].axvline(m, color = 'k', linestyle = '--')
+g.axes[1,0].axhline(c, color = 'k', linestyle = '--')
+g.axes[1,1].axvline(c, color = 'k', linestyle = '--')
+
+g.savefig('inference/fit_straight_line.pdf', dpi = 300)
