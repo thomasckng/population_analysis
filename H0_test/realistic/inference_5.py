d724b8c25ef64856dc3eaa5db3924d019d48641a
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-02-09 19:27:53 +0800

    Use mass model from git repo

--- H0_test/realistic/inference_5.py
+++ H0_test/realistic/inference_5.py
@@ -7,41 +7,8 @@ from figaro.mixture import DPGMM
 from figaro.utils import rejection_sampler, get_priors
 from figaro.cosmology import CosmologicalParameters
 from multiprocessing import Pool
-from numba import njit
-
-# Mass distribution
-# @njit
-def truncated_powerlaw(m, alpha, mmin, mmax):
-    p = m**-alpha * (alpha-1.)/(mmin**(1.-alpha)-mmax**(1.-alpha))
-    p[m < mmin] = 0.
-    p[m > mmax] = 0.
-    return p
-
-# @njit
-def smoothing(m, mmin, delta):
-    p = np.zeros(m.shape, dtype = np.float64)
-    p[m > mmin] = 1./(np.exp(delta/(m[m>mmin]-mmin) + delta/(m[m>mmin]-mmin-delta))+1)
-    p[m >= mmin + delta] = 1.
-    return p
-
-# @njit
-def powerlaw_unnorm(m, alpha, mmin, mmax, delta):
-    return truncated_powerlaw(m, alpha, mmin, mmax)*smoothing(m, mmin, delta)
-    
-# @njit
-def powerlaw(m, alpha, mmin, mmax, delta):
-    x  = np.linspace(mmin, mmax, 1000)
-    dx = x[1]-x[0]
-    n  = np.sum(powerlaw_unnorm(x, alpha, mmin, mmax, delta)*dx)
-    return powerlaw_unnorm(m, alpha, mmin, mmax, delta)/n
-
-# @njit
-def peak(m, mu, sigma):
-    return np.exp(-0.5*(m-mu)**2/sigma**2)/(np.sqrt(2*np.pi)*sigma)
-
-# @njit
-def plpeak(m, alpha=3.5, mmin=5, mmax=90, delta=5, mu=35, sigma=5, w=0.2):
-    return (1.-w)*powerlaw(m, alpha, mmin, mmax, delta) + w*peak(m, mu, sigma)
+# from numba import njit
+from population_models.mass import plpeak
 
 # dL distributions
 # @njit
@@ -64,8 +31,8 @@ def realistic_jsd(x, i):
 
 def scipy_minimize(i):
     return minimize(realistic_jsd,
-                    x0=[uniform(10,100), uniform(1,5), uniform(10,50), uniform(0,10), uniform(0,1)],
-                    bounds = ((10,100), (1,5), (10,50), (0,10), (0,1)), args=(i,)).x
+                    x0=[uniform(10,100), uniform(1.01,5), uniform(10,50), uniform(0.01,10), uniform(0,1)],
+                    bounds = ((10,100), (1.01,5), (10,50), (0.01,10), (0,1)), args=(i,)).x
 
 if __name__ == '__main__':
     true_H0 = 40. # km/s/Mpc (Deliberately off value)
--- H0_test/realistic/pp_plot.py
+++ H0_test/realistic/pp_plot.py
@@ -8,47 +8,14 @@ from figaro.cosmology import CosmologicalParameters
 from multiprocessing import Pool
 import sys
 import pickle
-from numba import njit
+# from numba import njit
+from population_models.mass import plpeak
 
 def reconstruct_observed_distribution(samples):
     mix = DPGMM([[M_min, M_max]], prior_pars=get_priors([[M_min, M_max]], samples))
     return mix.density_from_samples(samples)
 
 if __name__ == '__main__':
-    # Mass distribution
-    # @njit
-    def truncated_powerlaw(m, alpha, mmin, mmax):
-        p = m**-alpha * (alpha-1.)/(mmin**(1.-alpha)-mmax**(1.-alpha))
-        p[m < mmin] = 0.
-        p[m > mmax] = 0.
-        return p
-
-    # @njit
-    def smoothing(m, mmin, delta):
-        p = np.zeros(m.shape, dtype = np.float64)
-        p[m > mmin] = 1./(np.exp(delta/(m[m>mmin]-mmin) + delta/(m[m>mmin]-mmin-delta))+1)
-        p[m >= mmin + delta] = 1.
-        return p
-
-    # @njit
-    def powerlaw_unnorm(m, alpha, mmin, mmax, delta):
-        return truncated_powerlaw(m, alpha, mmin, mmax)*smoothing(m, mmin, delta)
-        
-    # @njit
-    def powerlaw(m, alpha, mmin, mmax, delta):
-        x  = np.linspace(mmin, mmax, 1000)
-        dx = x[1]-x[0]
-        n  = np.sum(powerlaw_unnorm(x, alpha, mmin, mmax, delta)*dx)
-        return powerlaw_unnorm(m, alpha, mmin, mmax, delta)/n
-
-    # @njit
-    def peak(m, mu, sigma):
-        return np.exp(-0.5*(m-mu)**2/sigma**2)/(np.sqrt(2*np.pi)*sigma)
-
-    # @njit
-    def plpeak(m, alpha=3.5, mmin=5, mmax=90, delta=5, mu=35, sigma=5, w=0.2):
-        return (1.-w)*powerlaw(m, alpha, mmin, mmax, delta) + w*peak(m, mu, sigma)
-
     # dL distribution
     # @njit
     def dLsq(dL, dLmax = 5000):
