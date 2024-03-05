d3b4fc19965202431ce0b803dc8996eca240cae6
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-03-05 09:29:31 +0800

    Updated pp_plot.py

--- H0_test/realistic/pp_plot.py
+++ H0_test/realistic/pp_plot.py
@@ -3,7 +3,6 @@ from tqdm import tqdm
 from scipy.spatial.distance import jensenshannon as scipy_jsd
 from figaro.mixture import DPGMM
 from figaro.utils import get_priors, rejection_sampler
-from scipy.stats import norm
 from figaro.cosmology import CosmologicalParameters
 from multiprocessing import Pool
 import sys
@@ -20,11 +19,11 @@ if __name__ == '__main__':
     # @njit
     def dLsq(dL, dLmax = 5000):
         return 3 * dL ** 2 / dLmax ** 3
-
-    true_H0 = 40. # km/s/Mpc (Deliberately off value)
-    true_omega = CosmologicalParameters(true_H0/100., 0.315, 0.685, -1., 0., 0.)
-    n_draws_samples = 2000
-    norm_dist = norm()
+    
+    n_runs = 100
+    
+    true_H0 = np.random.uniform(40, 100, n_runs)
+    true_omega = [CosmologicalParameters(true_H0[i]/100., 0.315, 0.685, -1., 0., 0.) for i in range(n_runs)]
 
     print("Preparing model pdfs...")
     mz = np.linspace(1,200,2000)
@@ -33,7 +32,7 @@ if __name__ == '__main__':
     H0 = np.linspace(20,60,500)
 
     def luminosity_distance_to_redshift(distance, H0s):
-        return np.array([CosmologicalParameters(H0/100, 0.315, 0.685, -1, 0, 0.).Redshift(distance) for H0 in H0s])
+        return np.array([CosmologicalParameters(H0/100., 0.315, 0.685, -1., 0., 0.).Redshift(distance) for H0 in H0s])
 
     z = luminosity_distance_to_redshift(dL, H0)
 
@@ -44,6 +43,7 @@ if __name__ == '__main__':
     model_pdf = model_pdf / np.trapz(model_pdf, mz, axis=0)
 
     print("Computing pp plot...")
+    n_draws_samples = 2000
     n_draws_figaro = 1000
     M_min = 0
     M_max = 200
@@ -53,28 +53,28 @@ if __name__ == '__main__':
     H0_samples_arr = []
     H0_perc_arr = []
     with Pool(64) as p:
-        for i in tqdm(range(100)):
-            i = 0
-            while i < 10:
+        for i in tqdm(range(n_runs)):
+            j = 0
+            while j < 10:
                 try:
                     # Generate samples from source distribution
                     valid = False
                     while not valid:
                         dL_sample = rejection_sampler(n_draws_samples, dLsq, [1,5000])
                         M_sample  = rejection_sampler(n_draws_samples, plpeak, [1,200])
-                        z_sample  = np.array([true_omega.Redshift(d) for d in dL_sample])
+                        z_sample  = np.array([true_omega[i].Redshift(d) for d in dL_sample])
                         Mz_sample = M_sample * (1 + z_sample)
                         valid = Mz_sample.max() < M_max and Mz_sample.min() > M_min
 
                     # Reconstruct observed distribution using FIGARO
                     draws = p.map(reconstruct_observed_distribution, np.full((n_draws_figaro,len(Mz_sample)), Mz_sample))
                 except Exception as e:
-                    i = i + 1
+                    j = j + 1
                     print("An exception occurred:", e)
                     continue # skip remaining code and try again
 
                 # Mask out mz where there is no sample
-                mask = [mz[i] <= np.max(Mz_sample) and mz[i] >= np.min(Mz_sample) for i in range(len(mz))]
+                mask = [mz[k] <= np.max(Mz_sample) and mz[k] >= np.min(Mz_sample) for k in range(len(mz))]
                 mz_short = mz[mask]
                 model_pdf_short = model_pdf[mask]
 
@@ -102,4 +102,4 @@ if __name__ == '__main__':
     jsd_arr = np.array(jsd_arr)
     H0_samples_arr = np.array(H0_samples_arr)
     H0_perc_arr = np.array(H0_perc_arr)
-    np.savez("result_pp_plot.npz", jsd_arr=jsd_arr, H0_samples_arr=H0_samples_arr, H0_perc_arr=H0_perc_arr)
+    np.savez("result_pp_plot.npz", true_H0=true_H0, jsd_arr=jsd_arr, H0_samples_arr=H0_samples_arr, H0_perc_arr=H0_perc_arr)
