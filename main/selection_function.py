ecd2d6d9e845ba85f60818dcbde0bbe40b3a10f7
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-04-30 15:39:42 +0200

    Test step selection function

--- main/selection_function.py
+++ main/selection_function.py
@@ -1,6 +1,14 @@
-# A test selection function
+import numpy as np
+
+# Selection function with linearly increasing probability from 0 to 1 as x increases from 0 to 100 and probability 1 for x>100
 def selection_function(x):
     p = x/100
     p[x>100]=1.
     p[x<0]=0.
     return p
+
+# # Selection function with probability 1 for x>=50 and 0.1 for x<50
+# def selection_function(x):
+#     p = np.ones(len(x))
+#     p[x.flatten()<50]=0.1
+#     return p
